7c17eac1531d3d5304d011af70f9690bdb720709
Jake O'Rielly <jake.orielly@gmail.com>
2019-10-15 18:55:29 -0400

    Simple 'character recognition' neural net

--- neural_net.py
+++ neural_net.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def dot(v, w):
     return sum(v_i * w_i for v_i, w_i in zip(v, w))
@@ -22,12 +23,122 @@ def feed_forward(neural_network, input_vector):
 
     return outputs
 
-xor_network = [
-    [[20, 20, -30],
-    [20, 20, -10]],
-    [[-60, 60, -30]]
-]
+def backpropagate(network, input_vector, targets):
+    hidden_outputs, outputs = feed_forward(network, input_vector)
 
-for x in [0, 1]:
-    for y in [0, 1]:
-        print(x,y,feed_forward(xor_network,[x,y])[-1])
+    # the output * (1 - output) is from the derivative of sigmoid
+    output_deltas = [output * (1 - output) * (output - target)
+                    for output, target in zip(outputs, targets)]
+
+    # adjust weights for output layer, one neuron at a time
+    for i, output_neuron in enumerate(network[-1]):
+        for j, hidden_output in enumerate(hidden_outputs + [1]):
+            # adjust the jth weight based on this neuron's delta and it's jth input
+            output_neuron[j] -= output_deltas[i] * hidden_output
+
+    # back-propagate errors to hidden layer
+    hidden_deltas = [hidden_output * (1 - hidden_output) * 
+                    dot(output_deltas, [n[i] for n in output_layer])
+                    for i, hidden_output in enumerate(hidden_outputs)]
+
+    # adjust weights for hidden layer, one neuron at a time
+    for i, hidden_neuron in enumerate(network[0]):
+        for j, input in enumerate(input_vector + [1]):
+            hidden_neuron[j] -= hidden_deltas[i] * input
+
+
+random.seed(0)
+input_size = 25
+num_hidden = 5
+output_size = 10
+
+# each hidden neuron has one weight per input, plus a bias weight
+hidden_layer = [[random.random() for _ in range(input_size + 1)]
+                for _ in range(num_hidden)]
+
+# each output neuron has one weight per hidden neuron, plus a bias weight
+output_layer = [[random.random() for _ in range(num_hidden + 1)]
+    for _ in range(output_size)]
+
+# network starts with random weights
+network = [hidden_layer, output_layer]
+
+targets = [[1 if i == j else 0 for i in range(10)] for j in range(10)]
+raw_digits = [
+          """11111
+             1...1
+             1...1
+             1...1
+             11111""",
+
+          """..1..
+             ..1..
+             ..1..
+             ..1..
+             ..1..""",
+
+          """11111
+             ....1
+             11111
+             1....
+             11111""",
+
+          """11111
+             ....1
+             11111
+             ....1
+             11111""",
+
+          """1...1
+             1...1
+             11111
+             ....1
+             ....1""",
+
+          """11111
+             1....
+             11111
+             ....1
+             11111""",
+
+          """11111
+             1....
+             11111
+             1...1
+             11111""",
+
+          """11111
+             ....1
+             ....1
+             ....1
+             ....1""",
+
+          """11111
+             1...1
+             11111
+             1...1
+             11111""",
+
+          """11111
+             1...1
+             11111
+             ....1
+             11111"""]
+
+def make_digit(raw_digit):
+    return [1 if c == '1' else 0
+            for row in raw_digit.split("\n")
+            for c in row.strip()]
+
+inputs = list(map(make_digit, raw_digits))
+
+def predict(input):
+    output = feed_forward(network, input)[-1]
+    return output.index(max(output))
+
+# 10k iterations
+for _ in range(10000):
+    for input_vector, target_vector in zip(inputs, targets):
+        backpropagate(network, input_vector, target_vector)
+
+print(predict(inputs[7]))
